829bb937fdb93b410629faddf2e6c51b621340c1
Matvey Prupas <matvey_prupas@epam.com>
2023-01-07 16:44:03 +0200

    style: add styles to Filters

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,14 +1,32 @@
+import './Filters.css';
 import { ReactElement } from "react";
 
 export const Filters = (): ReactElement => {
     return (
-      <div>
-        <input type='checkbox' id="dry" name="dry"/>
-        <label htmlFor="dry">dry</label>
-        <input type='checkbox' id="semi-dry" name="semi-dry"/>
-        <label htmlFor="semi-dry">semi-dry</label>
-        <input type='checkbox' id="sweet" name="sweet"/>
-        <label htmlFor="sweet">sweet</label>
+      <div className='filters'>
+        <label className='filters_label' htmlFor="dry">
+          <input type='checkbox' id="dry" name="dry"/>
+          <span className="filters_checkbox__outer">
+            <span className="filters_checkbox__inner"></span>
+          </span>
+          <span className='filters_text'>dry</span>
+        </label>
+        
+        <label className='filters_label' htmlFor="semi-dry">
+          <input type='checkbox' id="semi-dry" name="semi-dry"/>
+          <span className="filters_checkbox__outer">
+            <span className="filters_checkbox__inner"></span>
+          </span>
+          <span className='filters_text'>semi-dry</span>
+        </label>
+        
+        <label className='filters_label' htmlFor="sweet">
+          <input type='checkbox' id="sweet" name="sweet"/>
+          <span className="filters_checkbox__outer">
+            <span className="filters_checkbox__inner"></span>
+          </span>
+          <span className='filters_text'>sweet</span>
+        </label>
       </div>
     );
   }
